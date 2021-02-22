75dfd1a773b622e88d6370e692cda2fd0a7bb5f6
Futri <smfurdyna@gmail.com>
2021-02-22 20:13:52 +0100

    Automate the boring stuff. Chap 2

---
+++ Chap2_13.py
@@ -0,0 +1,11 @@
+#Write a short program that prints the numbers 1 to 10 using a for loop.
+#Then write an equivalent program that prints the numbers 1 to 10 using
+#a while loop.
+
+for i in range (1, 11):
+    print(i)
+
+j=1
+while j<11:
+    print(j)
+    j = j + 1
